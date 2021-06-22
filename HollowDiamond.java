9b5de6bcb1bb21dda92e06210464e8ef0e16289c
Eric <Eric@guigu.com>
2021-06-22 09:11:59 +0800

    第三次提交

--- HollowDiamond.java
+++ HollowDiamond.java
@@ -4,7 +4,7 @@ public class HollowDiamond {
             for (int j = 2; j <= 5-i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= 2*i-1; j++) {
+            for (int j = 3; j <= 2*i-1; j++) {
                 if(j==1||j==2*i-1){
                     System.out.print("*");
                 }else{
